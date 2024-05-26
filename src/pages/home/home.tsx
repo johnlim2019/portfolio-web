f38af169e702dd0bad92ddab9be94e361a7a616d
John Lim <69998939+johnlim2019@users.noreply.github.com>
2024-05-26 22:01:39 +0800

    update: portfolio stub

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,6 +4,7 @@ import "../../assets/containers.css"
 import "../../assets/titles.css"
 import TopBar from "../../components/TopBar/TopBar"
 import { TopBarEnum } from "../../enum/TopBarEnum"
+import RecentPostDisplay from "../../components/RecentPostDisplay/RecentPostDisplay"
 
 function HomePage(): JSX.Element {
 
@@ -13,9 +14,9 @@ function HomePage(): JSX.Element {
             <AspectRatio ratio={16 / 9} w='100%'>
                 <div className="homeTitleContainer">
                     <div className="homeTitleContainerShade">
-                        <Title tt="uppercase" className="homeTitleText1">John Lim</Title>
-                        <Center>
-                            <Divider color="white" w={500}></Divider>
+                        <Title tt="uppercase" fw={400} className="homeTitleText1">John Lim</Title>
+                        <Center p='1em'>
+                            <Divider color="white" w="60%"></Divider>
                         </Center>
                         <Title order={3} fw={100} tt="uppercase" className="homeTitleText2">Full Stack Web Developer</Title>
                     </div>
@@ -27,17 +28,27 @@ function HomePage(): JSX.Element {
             <Container className="responsiveContainer">
                 <Stack align="center">
                     <Center p="lg">
-                        <Text fs="italic">about</Text>
+                        <Text fs="italic">01 about</Text>
                     </Center>
                     <Text ta="center">
-                        Graduated in 2024 from SUTD's Computer Science and Design Programme with a<br />  Bachelor's of Engineering - Honours with Distinction.
+                        Graduated from SUTD class of 2024 with: <br /> Bachelor's of Engineering - Honours with Distinction.
                     </Text>
-
+                    <Text ta="center">Major: Computer Science and Design</Text>
+                    <Text ta="center">Minor: Artificial Intelligence</Text>
+                    <Text ta="center">Minor: Digital Humanities</Text>
+                    <Center p='lg'>
+                        <Divider orientation="vertical" color="white" h='5em'></Divider>
+                    </Center>
+                    <Center>
+                        <Text fs="italic">02 portfolio</Text>
+                    </Center>
+                    <Text ta='justify'>
+                        This portfolio encompasses diverse fields, ranging from culture to technology. This interdisciplinary background enables me to manage multimodal concepts effectively. It fosters a holistic perspective on how technology integrates into organisations and communities.
+                    </Text>
+                    <RecentPostDisplay />
                 </Stack>
-            </Container>
-            <Container className="tallContainer">
+            </Container >
 
-            </Container>
 
         </>
     )
