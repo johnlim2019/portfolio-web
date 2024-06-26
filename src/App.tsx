9b08b12b248352c3e8c98911b5164ce8b5eaa561
John Lim <69998939+johnlim2019@users.noreply.github.com>
2024-06-26 14:29:00 +0800

    update: capstone page route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import About from './pages/About/About'
 import WritingPage from './pages/Writing Page/WritingPage'
 import WritingHomePage from './pages/WritingHomePage/WritingHomePage'
 import ProjectHomePage from './pages/ProjectsHomePage/ProjectsHomePage'
+import CapstonePage from './pages/ProjectsPage/CapstonePage/CapstonePage'
 
 function App(): JSX.Element {
     return (
@@ -14,6 +15,7 @@ function App(): JSX.Element {
                 <Route path='/writings/:slug' element={<WritingPage />}></Route>
                 <Route path='/writings' element={<WritingHomePage />}></Route>
                 <Route path='/projects' element={<ProjectHomePage />}></Route>
+                <Route path='/projects/capstone-project' element={<CapstonePage />}></Route>
             </Routes>
         </>
     )
