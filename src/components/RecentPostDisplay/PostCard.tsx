adc07f4bd040790763e49c100790499fff6685be
johnlim2019 <69998939+johnlim2019@users.noreply.github.com>
2024-07-03 12:18:07 +0800

    update: new photos

--- src/components/RecentPostDisplay/PostCard.tsx
+++ src/components/RecentPostDisplay/PostCard.tsx
@@ -1,4 +1,4 @@
-import { Group, Image, Paper, Stack, Text, Title } from "@mantine/core";
+import { Container, Group, Image, Paper, Stack, Text, Title } from "@mantine/core";
 import { IPostCard } from "./Interfaces/IPostCard";
 import { Link } from "react-router-dom";
 import "../../assets/global.css"
@@ -11,7 +11,9 @@ function PostCard(props: IPostCard): JSX.Element {
         <Link to={props.slug} state={{ props: props }} >
             <Paper radius="md" className={styles.cardOuter} >
                 <Group grow className={styles.cardInner} justify='center'>
-                    <Image w="50%" className={styles.cardComponent} src={props.image}></Image>
+                    <Container className={styles.cardComponent} bg="white">
+                        <Image src={props.image}></Image>
+                    </Container>
                     <Stack w="40%" className={styles.cardComponent} justify='Flex-start'>
                         <Title ta="right" className='homeTitleText2'>{props.title}</Title>
                         <Title ta="right" className='homeTitleText3'>{props.date}</Title>
