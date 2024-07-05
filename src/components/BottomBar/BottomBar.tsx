9b9a2f77333ce924125ac42ef588117e3718c39d
johnlim2019 <69998939+johnlim2019@users.noreply.github.com>
2024-07-05 14:45:54 +0800

    update: bolierplate for bottom bar component

---
+++ src/components/BottomBar/BottomBar.tsx
@@ -0,0 +1,21 @@
+import { Container, Group } from '@mantine/core';
+import "../../assets/buttons.css";
+import "../../assets/titles.css";
+
+
+function BottomBar(): JSX.Element {
+
+
+    return (
+        <>
+            <Container size="xl" p='md'>
+                <Group justify='space-between'>
+
+                </Group >
+            </Container >
+
+        </>
+    )
+}
+
+export default BottomBar
